0406f84701ed510782a87e5aee7dff3712517ef5
August Lilleaas <august@augustl.com>
2014-12-28 23:43:43 +0100

    Needs moar threading macro.

--- src/olyp_central_api/datomic_util.clj
+++ src/olyp_central_api/datomic_util.clj
@@ -3,4 +3,6 @@
 
 (defn get-most-recent-t [ent]
   (let [db (d/entity-db ent)]
-    (d/tx->t (ffirst (d/q '[:find (max ?tx) :in $ ?eid :where [?eid _ _ ?tx]]  db (:db/id ent))))))
+    (-> (d/q '[:find (max ?tx) :in $ ?eid :where [?eid _ _ ?tx]] db (:db/id ent))
+        ffirst
+        d/tx->t)))
