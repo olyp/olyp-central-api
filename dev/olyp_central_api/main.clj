fd91f3ec6a79947b73047f83d11cfca07ceafe73
August Lilleaas <august@augustl.com>
2015-01-06 21:20:09 +0100

    Moving dev seed data creation to dev ns.

--- dev/olyp_central_api/main.clj
+++ dev/olyp_central_api/main.clj
@@ -1,7 +1,35 @@
 (ns olyp-central-api.main
   (:gen-class)
   (:require olyp-central-api.app
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [io.rkn.conformity :as conformity]
+            [datomic.api :as d]
+            [olyp-central-api.factories.user-factory :as user-factory]
+            [clojure.tools.logging :as log])
+  (:import [java.util UUID]))
+
+(defn generate-initial-seed-tx []
+  [{:db/id (d/tempid :db.part/user)
+    :reservable-room/public-id (d/squuid)
+    :reservable-room/name "Rom 5"}
+
+   ;; TODO: Only create this data in dev mode
+   {:db/id (d/tempid :db.part/user -1)
+    :customer/public-id (str (d/squuid))
+    :customer/type :customer.type/company
+    :customer/brreg-id "123456789"
+    :customer/name "Quentin Inc."
+    :customer/zip "1410"
+    :customer/city "Kolbotn"}
+
+   {:db/id (d/tempid :db.part/user)
+    :user/public-id (UUID/fromString "54a495b3-3a20-4d37-88bf-9a433d66db35")
+    :user/email "quentin@test.com"
+    :user/name "Quentin Test"
+    :user/customer (d/tempid :db.part/user -1)
+    :user/bcrypt-password (user-factory/encrypt-password "test")
+    :user/auth-token "78888a117972edc201892c53498321fa5bfeb31aec5e3bd722f127b1cb0c6757"}
+])
 
 (defn -main [& args]
   (let [app
@@ -9,5 +37,8 @@
          {:database {:type :datomic-mem}
           :web {:port 3000}}
          (olyp-central-api.app/create-system)
-         (component/start)
-         (def app))]))
+         (component/start))]
+    (log/info "Creating dev seed data")
+    (conformity/ensure-conforms
+     (get-in app [:database :datomic-conn])
+     {:olyp/initial-seed-data {:txes [(generate-initial-seed-tx)]}} [:olyp/initial-seed-data])))
--- src/olyp_central_api/components/datomic_connection.clj
+++ src/olyp_central_api/components/datomic_connection.clj
@@ -3,33 +3,10 @@
             [io.rkn.conformity :as conformity]
             [clojure.java.io :as io]
             [datomic.api :as d]
-            [clojure.tools.logging :as log]
-            [olyp-central-api.factories.user-factory :as user-factory])
+            [clojure.tools.logging :as log])
   (:import [java.util UUID]))
 
 (def schema (-> "datomic_schema.edn" io/resource slurp read-string))
-(defn generate-initial-seed-tx []
-  [{:db/id (d/tempid :db.part/user)
-    :reservable-room/public-id (d/squuid)
-    :reservable-room/name "Rom 5"}
-
-   ;; TODO: Only create this data in dev mode
-   {:db/id (d/tempid :db.part/user -1)
-    :customer/public-id (str (d/squuid))
-    :customer/type :customer.type/company
-    :customer/brreg-id "123456789"
-    :customer/name "Quentin Inc."
-    :customer/zip "1410"
-    :customer/city "Kolbotn"}
-
-   {:db/id (d/tempid :db.part/user)
-    :user/public-id (UUID/fromString "54a495b3-3a20-4d37-88bf-9a433d66db35")
-    :user/email "quentin@test.com"
-    :user/name "Quentin Test"
-    :user/customer (d/tempid :db.part/user -1)
-    :user/bcrypt-password (user-factory/encrypt-password "test")
-    :user/auth-token "78888a117972edc201892c53498321fa5bfeb31aec5e3bd722f127b1cb0c6757"}
-])
 
 (defrecord Database [connection-uri]
   component/Lifecycle
@@ -40,7 +17,6 @@
     (let [datomic-conn (d/connect connection-uri)]
       (log/info (str "Ensuring Datomic conforms to schema"))
       (conformity/ensure-conforms datomic-conn schema [:olyp/main-schema])
-      (conformity/ensure-conforms datomic-conn {:olyp/initial-seed-data {:txes [(generate-initial-seed-tx)]}} [:olyp/initial-seed-data])
       (assoc component
         :connection-uri connection-uri
         :datomic-conn datomic-conn)))
