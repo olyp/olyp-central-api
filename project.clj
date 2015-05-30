32a90d242795cc9e6fdb37b9d11226ba5c39c7a9
August Lilleaas <august@augustl.com>
2015-05-30 23:06:51 +0200

    Making project load properly in repl.

--- project.clj
+++ project.clj
@@ -16,8 +16,7 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.2"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[reloaded.repl "0.1.0"]]
-                   :main olyp-central-api.main-dev}
+                   :dependencies [[reloaded.repl "0.1.0"]]}
              :uberjar {:source-paths ["prod"]
                        :main olyp-central-api.main-prod
                        :aot [olyp-central-api.main-prod]
